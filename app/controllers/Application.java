0e639a2045740314d013797fa8d3c594cacda8d7
Rémy Elazare <remy.elazare@gmail.com>
2013-06-11 13:41:33 -0400

    up http://www.playframework.com/documentation/2.1.1/JavaGuide3 - part1

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.Project;
+import models.Task;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
@@ -7,7 +9,6 @@ import views.html.index;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(Project.find.all(), Task.find.all()));
     }
-
 }
