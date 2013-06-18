0711a37270c78b0b83b53ed8537abe2d932f31d8
remylab <remy.elazare@gmail.com>
2013-06-17 21:59:42 -0400

    style + http://www.playframework.com/documentation/2.1.1/JavaGuide4 -part1

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,11 +4,14 @@ import models.Project;
 import models.Task;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render(Project.find.all(), Task.find.all()));
-    }
+	public static Result index() {
+		return ok(views.html.index.render(Project.find.all(), Task.find.all()));
+	}
+
+	public static Result login() {
+		return ok(views.html.login.render());
+	}
 }
