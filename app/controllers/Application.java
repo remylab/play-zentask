c9ffd63f9ee723b5193fc792b0d1881e3c71e6f2
Rémy Elazare <remy.elazare@gmail.com>
2013-06-20 13:26:37 -0400

    add logout action

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,15 +2,23 @@ package controllers;
 
 import models.Project;
 import models.Task;
+import models.ZenUser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 
 public class Application extends Controller {
 
-	@Security.Authenticated(Secured.class)
-	public static Result index() {
-		return ok(views.html.index.render(Project.find.all(), Task.find.all()));
-	}
+    @Security.Authenticated(Secured.class)
+    public static Result index() {
+        return ok(views.html.index.render(Project.findInvolving(request().username()), Task.findTodoInvolving(request().username()), ZenUser.find.byId(request().username())));
+
+    }
+
+    public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(routes.Login.index());
+    }
 
 }
