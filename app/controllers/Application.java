37d7364b8750e732cc93c478c9d9c3e650b66329
remylab <remy.elazare@gmail.com>
2013-06-19 21:59:43 -0400

    add authenticator

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,11 +4,13 @@ import models.Project;
 import models.Task;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(views.html.index.render(Project.find.all(), Task.find.all()));
-    }
+	@Security.Authenticated(Secured.class)
+	public static Result index() {
+		return ok(views.html.index.render(Project.find.all(), Task.find.all()));
+	}
 
 }
--- test/controllers/LoginTest.java
+++ test/controllers/LoginTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static play.test.Helpers.callAction;
 import static play.test.Helpers.fakeRequest;
+import static play.test.Helpers.header;
 import static play.test.Helpers.session;
 import static play.test.Helpers.status;
 
@@ -37,5 +38,18 @@ public class LoginTest extends AbstractDBApplicationTest {
 		Result result = callAction(controllers.routes.ref.Login.authenticate(), fakeRequest().withFormUrlEncodedBody(ImmutableMap.of("email", "bob@example.com", "password", "badpassword")));
 		assertEquals(400, status(result));
 		assertNull(session(result).get("email"));
+	}
+
+	@Test
+	public void authenticated() {
+		Result result = callAction(controllers.routes.ref.Application.index(), fakeRequest().withSession("email", "bob@example.com"));
+		assertEquals(200, status(result));
+	}
+
+	@Test
+	public void notAuthenticated() {
+		Result result = callAction(controllers.routes.ref.Application.index(), fakeRequest());
+		assertEquals(303, status(result));
+		assertEquals("/login", header("Location", result));
 	}
 }
